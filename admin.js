9742b7e36a3847dbbed01974431465b539491703
knguynnn <phamkhanhnguyen2006@gmail.com>
2025-10-22 20:15:05 +0700

    kkk

--- admin.js
+++ admin.js
@@ -5,6 +5,28 @@ function openLogin(){
 }
 function closeLogin(){
     let overlay=document.getElementById("loginOverlay")
-    overlay.style.display="none";
+    overlay.classList.remove("show");
     setTimeout(() => overlay.style.display = "none", 300);
 }
+function account(){
+    let overlay=document.getElementById("loginOverlay")
+    overlay.classList.remove("show");
+    setTimeout(() => overlay.style.display = "none", 300);
+}
+
+function loginSuccess() {
+    // Ẩn nút "Đăng nhập"
+    document.getElementById("loginButton").style.display = "none";
+    // Hiện nút "Tài khoản"
+    document.getElementById("accountButton").style.display = "block";
+    // Ẩn form đăng nhập (với hiệu ứng)
+    closeLogin();
+}
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".login form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // chặn reload trang
+        // giả sử đăng nhập thành công
+        loginSuccess();
+    });
+});
